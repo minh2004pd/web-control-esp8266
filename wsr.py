318d1f577dbee0215fb9ba2f19a1f26af5410d79
Doan Duc Minh <114096378+minh2004pd@users.noreply.github.com>
2023-04-23 15:47:52 +0700

    second

--- wsr.py
+++ wsr.py
@@ -1,111 +1,165 @@
 from flask import Flask, render_template, request
-import speech_recognition as sr
-import pyautogui
+import datetime
 import requests
-import os
 
 app = Flask(__name__)
 
-url0 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V0="
-url1 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V1="
-url2 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V2="
-
-# thiet lap tham so chunk_duration
-chunk_duration = 5 # thoi gian moi chunk(giay)
-
-record_duration = 5 # thoi gian ghi am(giay)
+den1 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V0="
+den2 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V3="
+quat0 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V1="
+quat1 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V4="
+quat2 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V5="
+quat3 = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V6="
+cauDao = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V7="
+cua = "https://sgp1.blynk.cloud/external/api/update?token=rAG4ADhrMioce3oAk8IdiVXMcqmoMvQl&V2="
 
 @app.route("/")
 def index():
     return render_template("b.html")
 
-@app.route('/bat_den')
-def bat_den():
-    url = url0 + "1"
+@app.route("/mo_cau_dao")
+def mo_cau_dao():
+    url = cauDao + "1"
     requests.get(url)
-    pyautogui.press('q')
-    return "Đèn đã được bật"
+    return  " "
 
-@app.route('/tat_den')
-def tat_den():
-    url = url0 + "0"
+@app.route("/dong_cau_dao")
+def dong_cau_dao():
+    url = cauDao + "0"
     requests.get(url)
-    pyautogui.press('q')
-    return "Đèn đã được tắt"
+    return  " "
+
+@app.route("/bat_quat")
+def bat_quat():
+    url = quat0 + "1"
+    requests.get(url)
+    return " "
+
+@app.route("/tat_quat")
+def tat_quat():
+    url = quat0 + "0"
+    requests.get(url)
+    return " "
+
+@app.route("/nac_1")
+def nac_1():
+    url = quat1 + "1"
+    requests.get(url)
+    return " "
+
+@app.route("/tat_nac_1")
+def tat_nac_1():
+    url = quat1 + "0"
+    requests.get(url)
+    return " "
+
+@app.route("/nac_2")
+def nac_2():
+    url = quat2 + "1"
+    requests.get(url)
+    return " "
+
+@app.route("/tat_nac_2")
+def tat_nac_2():
+    url = quat2 + "0"
+    requests.get(url)
+    return " "
+
+@app.route("/nac_3")
+def nac_3():
+    url = quat3 + "1"
+    requests.get(url)
+    return " "
+
+@app.route("/tat_nac_3")
+def tat_nac_3():
+    url = quat3 + "0"
+    requests.get(url)
+    return " "
+
+@app.route('/bat_den1')
+def bat_den1():
+    url = den1 + "1"
+    requests.get(url)
+    return "Đèn 1 đã được bật"
+
+@app.route('/tat_den1')
+def tat_den1():
+    url = den1 + "0"
+    requests.get(url)
+    return "Đèn 1 đã được tắt"
+
+@app.route('/bat_den2')
+def bat_den2():
+    url = den2 + "1"
+    requests.get(url)
+    return "Đèn 2 đã được bật"
+
+@app.route('/tat_den2')
+def tat_den2():
+    url = den2 + "0"
+    requests.get(url)
+    return "Đèn 2 đã được tắt"
 
 @app.route('/mo_cua')
 def mo_cua():
-    url = url1 + "1"
+    url = cua + "1"
     requests.get(url)
-    pyautogui.press('q')
     return "Cửa đã được mở"
 
 @app.route('/dong_cua')
 def dong_cua():
-    url = url1 + "0"
+    url = cua + "0"
     requests.get(url)
-    pyautogui.press('q')
     return "Cửa đã được đóng"
 
 @app.route('/nhap_lenh', methods=['POST'])
 def nhap_lenh():
     text = request.form['text-input']
     file = open('voice.txt','a+',encoding='utf-8')
-    file.write(text)
-    if 'bật đèn' in text:
-        return bat_den()
-    elif 'tắt đèn' in text:
-        return tat_den()
+
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    file.write(formatted_time + " - " + text + "\n")
+    file.close()
+    if 'bật đèn 1' in text:
+        return bat_den1()
+    elif 'tắt đèn 1' in text:
+        return tat_den1()
     elif 'mở cửa' in text:
         return mo_cua()
     elif 'đóng cửa' in text:
         return dong_cua()
+    elif 'bật đèn 2' in text:
+        return bat_den2()
+    elif 'tắt đèn 2' in text:
+        return tat_den2()
+    elif 'bật quạt' in text:
+        return bat_quat()
+    elif 'tắt quạt' in text:
+        return tat_quat()
+    elif 'quạt nấc 1' in text:
+        return nac_1()
+    elif 'tắt nấc 1' in text:
+        return tat_nac_1()
+    elif 'quạt nấc 2' in text:
+        return nac_2()
+    elif 'tắt nấc 2' in text:
+        return tat_nac_2()
+    elif 'quạt nấc 3' in text:
+        return nac_3()
+    elif 'tắt nấc 3' in text:
+        return tat_nac_3()
+    elif 'mở cầu dao' in text:
+        return mo_cau_dao()
+    elif 'đóng cầu dao' in text:
+        return dong_cau_dao()
+    
     else:
         message = "Không tìm thấy lệnh"
         return render_template('a.html', message=message)
-    # r = sr.Recognizer()
-    # audio_file = request.files["audio"]
-    # audio_file.save("name.wav")
-    # # test_file = audio_file
-    # audio_data = b''
-    # chunk_size = 4096 # adjust this to your needs
-    # audio_format = mimetypes.guess_extension(audio_file.content_type)
 
-    # while True:
-    #     chunk = audio_file.read(chunk_size)
-    #     if not chunk:
-    #         break
-    #     audio_data += chunk
-    # audio = sr.AudioData(audio_data, sample_rate=16000, sample_width=2)
-    # print(audio)
-
-    # print(f"Received audio file: {audio_file.filename}")
-    # print(f"Received audio file in {audio_format} format.")
-    # print(f"Audio data length: {len(audio_data)} bytes")
-    
-    # with sr.AudioFile('myFile.wav') as source:
-    # # Thu âm nguồn từ microphone hoặc tệp
-    #     audio_text = r.listen(source)
-    #     print(audio_text)
-    #     try:
-    #         text = r.recognize_google(audio_text, language="vi-VN")
-    #         file = open("voice.txt", "w", encoding="utf-8")
-    #         file.write(text)
-    #         file.close()
-    #         if 'stop' in text:
-    #             return bat_den()
-    #         elif 'tắt đèn' in text:
-    #             return tat_den()
-    #         elif 'mở cửa' in text:
-    #             return mo_cua()
-    #         elif 'đóng cửa' in text:
-    #             return dong_cua()
-    #         else:
-    #             message = "Không tìm thấy lệnh"
-    #             return render_template('a.html', message=message)
-    #     except sr.UnknownValueError:
-    #         message = "Không nhận dạng được giọng nói"
-    #         return "Không nhận dạng được giọng nói"
     
 if __name__ == '__main__':
     
